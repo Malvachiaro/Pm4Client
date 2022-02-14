c9d03b8bf0d06b0c66b36e7e4cbab7a2bd82c0eb
Malvachiaro <giacomoscarpellini@gmail.com>
2022-02-14 19:32:46 +0100

    Some changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+import { hello } from "./pm4Lib/test1";
+
+
+</script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation {hello()}</p>
